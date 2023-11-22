42b3593a4eeb79c0b587fb24b8629af1b354037d
Jack-170 <agavejack@hotmail>
2023-11-22 17:37:33 +0100

    email: add event listener and html

--- mail/js/script.js
+++ mail/js/script.js
@@ -2,11 +2,23 @@
 const listaAccesso = ["email1@hotmail.com", "email2@hotmail.com", "email3@hotmail.com", "email4@hotmail.com", "email5@hotmail.com", "email6@hotmail.com", "email7@hotmail.com"];
 
 // Chiedi all'utente la sua email
-const userEmail = prompt("Inserisci la tua email:");
+// const userEmail = prompt("Inserisci la tua email:");
 
 // Controlla se l'email è nella lista di accesso
-if (listaAccesso.includes(userEmail)) {
-    console.log("Benvenuto!");
-} else {
-    console.log("Spiacenti, l'email non è in lista.");
-}
+
+const verifyButton = document.getElementById("verify");
+
+verifyButton.addEventListener("click",
+    function verificaAccesso() {
+        const userEmail = document.getElementById("userEmail").value;
+        const risultato = document.getElementById("risultato");
+    
+        if (listaAccesso.includes(userEmail)) {
+        risultato.textContent = "Benvenuto! Puoi accedere.";
+        risultato.style.color = "green";
+        } else {
+        risultato.textContent = "Spiacenti, l'accesso non è consentito.";
+        risultato.style.color = "red";
+        }
+    }
+);
